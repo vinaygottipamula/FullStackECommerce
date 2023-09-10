4cfe0d7e423e588dd14e6327d8b2696986ca74cd
Gottipamula Vinay <vinaygottipamula@gmail.com>
2023-09-10 23:33:09 +0530

    ItemsAdded

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useEffet, useState } from "react";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Box, Typography, Tab, Tabs, useMediaQuery } from '@mui/material';
 import Item from "../../components/Item";
-import {setItems} from '../../state';
+import { setItems } from '../../state';
 
 
 const ShoppingList = () => {
     const dispatch = useDispatch();
     const [value, setValue] = useState("all");
-    const items = useSelector((state) => state.cart.items);
-    console.log("🚀 ~ file: ShoppingList.jsx:12 ~ ShoppingList ~ items:", items)
-    const isNonMobile = useMediaQuery("(mid-width:600px)");
+    const items = useSelector((state) => state.cart.items);    
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+    console.log("items:", items)
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -26,10 +26,63 @@ const ShoppingList = () => {
 
     useEffect(() => {
         getItems();
-    }, []) //eslint-disable-line react-hooks/exhaustive-deps
-    return (
-        <div>shopping list</div>
-    )
+    }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
+    const topRatedItems = items.filter(
+        (item) => item.attributes.category === "topRated"
+    );
+    const newArrivalsItems = items.filter(
+        (item) => item.attributes.category === "newArrivals"
+    );
+    const bestSellersItems = items.filter(
+        (item) => item.attributes.category === "bestSellers"
+    );
+    return <Box width="80%" margin="80px auto">
+        <Typography variant="h3" textAlign="center">
+            Our Featured <b>Products</b>
+        </Typography>
+        <Tabs
+            textColor="primary"
+            indicatorColor="primary"
+            value={value}
+            onChange={handleChange}
+            centered
+            TabIndicatorProps={{ sx: { display: isNonMobile ? "block" : "none" }}}
+            sx={{
+                m: "25px",
+                "& .MuiTabs-flexContainer": {
+                    flexWrap: "wrap"
+                }
+            }}
+        >
+            <Tab label="ALL" value="all" />
+            <Tab label="NEW ARRIVALS" value="newArrivals" />
+            <Tab label="BEST SELLERS" value="bestSellers" />
+            <Tab label="TOP RATED" value="topRated" />
+        </Tabs>
+        <Box
+            margin="0 auto"
+            display="grid"
+            gridTemplateColumns="repeat(auto-fill,300px)"
+            justifyContent="space-around"
+            rowGap="20px"
+            columnGap="1.33%"
+        >
+            {value === "all" && items.map((item) => (
+                <Item item={item} key={`${item.name}-${item.id}`}/>
+            ))}
+            {value === "newArrivals" && newArrivalsItems.map((item) => (
+                <Item item={item} key={`${item.name}-${item.id}`}/>
+            ))}
+            {value === "bestSellers" && bestSellersItems.map((item) => (
+                <Item item={item} key={`${item.name}-${item.id}`}/>
+            ))}
+            {value === "topRated" && topRatedItems.map((item) => (
+                <Item item={item} key={`${item.name}-${item.id}`}/>
+                ))}
+        </Box>
+    </Box>;
+
 };
 
 export default ShoppingList;
